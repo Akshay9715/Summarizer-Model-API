2b746129fbe5ec54b41d046af9ab312b8ac9441c
Akshay Choudhary <153769403+Akshay9715@users.noreply.github.com>
2025-02-02 22:49:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def home():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
